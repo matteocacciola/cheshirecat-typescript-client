7e10c36e729e48072662dc9dda8ce2e9d60dfc95
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-12-21 21:26:07 +0100

    readiness and liveness health endpoint

--- src/endpoints/healthCheck.ts
+++ src/endpoints/healthCheck.ts
@@ -7,8 +7,19 @@ export class HealthCheckEndpoint extends AbstractEndpoint {
      *
      * @returns A message indicating that the server is running.
      */
-    async home(): Promise<any> {
-        const response = await this.client.getHttpClient().createHttpClient().get("/");
+    async liveness(): Promise<any> {
+        const response = await this.client.getHttpClient().createHttpClient().get("/health/liveness");
+        return response.data;
+    }
+
+    /**
+     * This endpoint is used to check the health of the CheshireCat server. It returns a simple message indicating that
+     * the server is running.
+     *
+     * @returns A message indicating that the server is running.
+     */
+    async readiness(): Promise<any> {
+        const response = await this.client.getHttpClient().createHttpClient().get("/health/readiness");
         return response.data;
     }
 }
