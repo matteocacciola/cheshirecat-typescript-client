9792aca6739823369aa9f7116038ebdeb3b5d7a3
Matteo Cacciola <matteo.cacciola@gmail.com>
2025-12-24 16:33:25 +0100

    feat: me endpoint

--- src/models/api/tokens.ts
+++ src/models/api/tokens.ts
@@ -1,5 +1,10 @@
+import {SerializedName} from "../../decorators";
+
 export class TokenOutput {
+    @SerializedName("access_token")
     accessToken: string;
+
+    @SerializedName("token_type")
     tokenType?: string | null = "Bearer";
 }
 
@@ -10,14 +15,23 @@ export class User {
 }
 
 export class AgentMatch {
+    @SerializedName("agent_id")
     agentId: string;
+
+    @SerializedName("agent_name")
     agentName: string;
+
+    @SerializedName("agent_description")
     agentDescription?: string | null = null;
+
     user: User;
 }
 
 export class MeOutput {
     success: boolean;
+
     agents: AgentMatch[];
+
+    @SerializedName("auto_selected")
     autoSelected: boolean;
 }
