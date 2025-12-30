e07c554c1006c0d1e3c783ad3642171c08c1250e
matteocacciola <matteo.cacciola@gmail.com>
2025-12-30 20:19:11 +0100

    Refactor MessageOutput class to remove deprecated content
    Removed deprecated 'content' property and its assignment in the constructor.

--- src/models/api/messages.ts
+++ src/models/api/messages.ts
@@ -4,19 +4,12 @@ export class MessageOutput extends MessageBase {
     type?: string | null = "chat";
     why?: Why | null = null;
     error?: boolean = false;
-    readonly content: string; // deprecated
-
-    constructor(public text: string = "", public image: string | null = null) {
-        super(text, image);
-        this.content = text;
-    }
 
     toArray(): Record<string, any> {
         return {
             ...super.toArray(),
             type: this.type,
             why: this.why?.toArray(),
-            content: this.text,
             error: this.error,
         };
     }
